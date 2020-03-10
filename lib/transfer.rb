0a307a19b1a2f4af1e8f1fa0c898ec4c3dc99113
Tim Donoghue <tmd112593@gmail.com>
2020-03-10 19:35:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if @status == "complete"
+    if @status == "rejected"
       @sender.deposit( @amount ) 
       @receiver.deposit( @amount * -1)
       @status = "reversed"
